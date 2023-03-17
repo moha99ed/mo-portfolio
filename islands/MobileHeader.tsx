97997f6a1ba6b69aa019ee1cc2e5fc76d5a987b8
mohammed <mohammedsebbagh@gmail.com>
2023-03-17 12:24:37 -0400

    me

--- islands/MobileHeader.tsx
+++ islands/MobileHeader.tsx
@@ -21,25 +21,16 @@ export default function MobileHeader(props: Omit<HeaderProps, "sticky">) {
         </div>
         <div class="flex min-h-screen items-center justify-center">
           <div
-            class="group relative my-2 mx-auto h-6 w-6 cursor-pointer border rounded text-blue-600 hover:bg-yellow-400 focus:outline-none"
+            class="flex items-center px-3 py-2 border rounded text-blue-500 hover:border-yellow-400 hover:bg-yellow-400 focus:outline-none"
             onClick={() => (checked.value = !checked.value)}
           >
             <svg
+              class="fill-current h-5 w-5"
+              viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
-              class={`icon icon-tabler icon-tabler-menu-2 absolute top-0 transition ${
-                checked.value ? "rotate-90 opacity-0" : "rotate-0 opacity-100"
-              }`}
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
             >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M4 6l16 0m-16 6l16 0m-16 6l16 0"></path>
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </div>
         </div>
